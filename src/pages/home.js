669117517d265db33eca0a656403df7da152805d
Claire Bain <claire.bain3@gmail.com>
2023-05-10 15:09:54 -0700

    gets art displaying

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,35 @@
-export default function Homepage() {
-    return <div>Homepage</div>
+import Image from 'next/image';
+import Art from './/../assets/artdata.json';
+
+
+// import Pic from './/../assets/thecrow.jpg';
+
+// import Pic from './../assets/thecrow.jpg'
+
+function Display(item) {
+    return(
+<div>
+    <div className = 'defaultArt'>
+    <h4 className='defaultArtTxt'>{item.name}</h4>
+<Image className = 'defaultArtImg'
+    src={item.pic} // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt={item.name}
+  />
+  </div>
+</div>
+
+    )
+}
+
+export default function Homepage(props) {
+
+
+    console.log(Art)
+    return <div>
+
+
+{Art.map(Display)}
+    </div>
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import React, { useState } from 'react';
 import { Inter } from 'next/font/google'
 import Painting from './painting';
@@ -49,40 +50,40 @@ export default function Home() {
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#home" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page"
+          <Link href="#home" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page"
                     onClick={() => handlePageChange('Homepage')}
 
                     className={currentPage === 'Homepage' ? 'nav-link active' : 'nav-link'}
-          >Home</a>
+          >Home</Link>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          <Link href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     onClick={() => handlePageChange('Digital')}
 
                     className={currentPage === 'Digital' ? 'nav-link active' : 'nav-link'}
-          >Digital Art</a>
+          >Digital Art</Link>
         </li>
         <li>
-          <a href="#painting" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          <Link href="#painting" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           onClick={() => handlePageChange('Painting')}
 
           className={currentPage === 'Painting' ? 'nav-link active' : 'nav-link'}
           >Painting
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          <Link href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     onClick={() => handlePageChange('Fiber')}
 
                     className={currentPage === 'Fiber' ? 'nav-link active' : 'nav-link'}
-          >Fiber Art</a>
+          >Fiber Art</Link>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          <Link href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     onClick={() => handlePageChange('Contact')}
 
                     className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-          >Contact</a>
+          >Contact</Link>
         </li>
       </ul>
     </div>
